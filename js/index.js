c5ca390286cc2e745aa8eee92f15148dd0062bfe
netogramacho <neto.paulo.po@gmail.com>
2021-07-04 20:59:59 -0300

    projeto finalizado

--- js/index.js
+++ js/index.js
@@ -1,3 +1,59 @@
+var interval;
+
+
 function abrirCheckout() {
     window.location.href = "https://payment.hotmart.com/Q53278902U?checkoutMode=10";
+}
+
+$(document).ready(function() {
+
+    numVagas = localStorage.getItem('numVagas');
+
+    if (numVagas == undefined) {
+        numVagas = 422;
+        localStorage.setItem('numVagas', numVagas);
+    }
+
+    controlarVagas(localStorage.getItem('numVagas'));
+
+
+    interval = setInterval(() => {
+        controlarVagas(localStorage.getItem('numVagas'));
+    }, 5000);
+
+    if (window.innerWidth < 768) {
+        setTimeout(() => {
+            slickImgs();
+        }, 500);
+    }
+
+});
+
+
+function controlarVagas(nv) {
+    nv--;
+
+    if (nv >= 0) {
+        localStorage.setItem('numVagas', nv);
+        $('.num-vagas').html(nv);
+    }
+
+    if (nv <= 0) {
+        clearInterval(interval);
+        $('.num-vagas').html('0');
+    }
+}
+
+function slickImgs() {
+    $('.grade-fotos').slick({
+        arrows: false
+    });
+}
+
+function nextSlick() {
+    $('.grade-fotos').slick('slickNext')
+}
+
+function prevSlick() {
+    $('.grade-fotos').slick('slickPrev');
 }
